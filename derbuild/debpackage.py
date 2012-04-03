d712aedfe39262802a39a5bb7c1eb968cc2a0e76
Dmitry Rozhkov <dmitry.rojkov@gmail.com>
2012-04-03 21:36:01 +0300

    use deb822 instead rfc822

--- derbuild/debpackage.py
+++ derbuild/debpackage.py
@@ -1,5 +1,5 @@
 import logging
-import email
+import deb822
 
 from derbuild import DerbuildError
 
@@ -14,11 +14,11 @@ class DebPackage(object):
         self.dsc_path = path
         self.workdir = workdir
         with open(path) as dsc_h:
-            dsc = email.message_from_file(dsc_h)
+            dsc = deb822.Dsc(dsc_h)
         self.name = dsc['source']
         self.version = dsc['version']
         self.binaries = [b.strip() for b in dsc['binary'].split(',')]
-        self.depends = [dep.split(' ', 1)[0].strip()
+        self.depends = [dep.split()[0].strip()
                         for dep in dsc['build-depends'].split(',')]
 
     def unpack(self):
